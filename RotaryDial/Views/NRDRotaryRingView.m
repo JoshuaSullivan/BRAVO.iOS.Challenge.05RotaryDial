a8b9a607e9d36bf8cf3a545cc1e028004c0b9d3a
Joshua Sullivan <jsulliva@nerdery.com>
2014-03-24 17:12:04 -0500

    Adding code comments.

--- RotaryDial/Views/NRDRotaryRingView.m
+++ RotaryDial/Views/NRDRotaryRingView.m
@@ -11,18 +11,44 @@
 
 const CGFloat kOuterRadiusMultiplier = 1.0f;
 const CGFloat kInnerRadiusMultiplier = 0.6f;
-const CGFloat kKnobArcAngle = M_PI / 8.0f;
+const CGFloat kKnobArcAngle = (float)M_PI / 8.0f;
 
+typedef NS_ENUM(NSInteger, RotaryRingTouchRegion) {
+
+    /** The touch point is outside the ring and outside the knob. */
+    RotaryRingTouchRegionOutside = 0,
+
+    /** The touch point is inside the knob. */
+    RotaryRingTouchRegionInsideKnob,
+
+    /** The touch point is inside the ring, clockwise from the knob. */
+    RotaryRingTouchRegionClockwiseFromKnob,
+
+    /** The touch point is inside the ring, couterclockwise from the knob. */
+    RotaryRingTouchRegionCounterclockwiseFromKnob
+};
 
 @interface NRDRotaryRingView () {
+    /** The calculated outer radius of the ring and knob. */
     CGFloat _outerRadius;
+
+    /** The calcualted inner radius of the ring and knob. */
     CGFloat _innerRadius;
+
+    /** The center point from which the knob and ring are drawn. */
     CGPoint _centerPoint;
+
+    /** The bounding box of the outer edge of the ring. */
     CGRect _outerBounds;
+
+    /** The bonding box of the inner edge of the ring. */
     CGRect _innerBounds;
 }
 
+/** The fill color for the ring. */
 @property (strong, nonatomic) UIColor *ringFillColor;
+
+/** The fill color for the knob. */
 @property (strong, nonatomic) UIColor *knobFillColor;
 
 @end
@@ -45,10 +71,17 @@ const CGFloat kKnobArcAngle = M_PI / 8.0f;
 
 - (void)commonInitWithFrame:(CGRect)frame
 {
+    // We want the biggest circle that fits inside the bounds of this view.
     CGFloat shortSide = fminf(frame.size.width, frame.size.height);
+
+    // Calculate the inner and outer radii
     _outerRadius = shortSide / 2.0f * kOuterRadiusMultiplier;
     _innerRadius = shortSide / 2.0f * kInnerRadiusMultiplier;
+
+    // Store the center point of the rings
     _centerPoint = CGPointMake(frame.size.width / 2.0f, frame.size.height / 2.0f);
+
+    // Calculate components needed to create the bounding frames for the inner and outer ring edges.
     CGFloat w0 = shortSide * kOuterRadiusMultiplier;
     CGFloat w1 = shortSide * kInnerRadiusMultiplier;
     CGFloat x0 = (frame.size.width - w0) / 2.0f;
@@ -57,7 +90,11 @@ const CGFloat kKnobArcAngle = M_PI / 8.0f;
     CGFloat y1 = (frame.size.height - w1) / 2.0f;
     _outerBounds = CGRectMake(x0, y0, w0, w0);
     _innerBounds = CGRectMake(x1, y1, w1, w1);
-    self.currentAngle = -M_PI_2;
+
+    // The initial postion of the knob is "North".
+    self.currentAngle = (float)-M_PI_2;
+
+    // Set the fill colors for the ring and knob.
     self.ringFillColor = [UIColor colorWithWhite:0.8f alpha:1.0f];
     self.knobFillColor = [UIColor colorWithWhite:0.4f alpha:1.0f];
 }
@@ -68,6 +105,7 @@ const CGFloat kKnobArcAngle = M_PI / 8.0f;
     CGContextSaveGState(c);
     
     // Clear the old drawing
+    CGContextSetFillColorWithColor(c, [UIColor clearColor].CGColor);
     CGContextClearRect(c, _outerBounds);
     
     // Draw the ring
@@ -101,6 +139,16 @@ const CGFloat kKnobArcAngle = M_PI / 8.0f;
         _currentAngle = currentAngle;
         [self setNeedsDisplay];
     }
+}
+
+#pragma mark - Touch helper methods
+
+- (RotaryRingTouchRegion)calculateTouchRegionForPoint:(CGPoint)touchPoint
+{
+    CGPoint radialPoint = CGPointMake(touchPoint.x - _centerPoint.x, touchPoint.y - _centerPoint.y);
+    CGFloat radius = sqrtf(powf(radialPoint.x, 2.0f) + powf(radialPoint.y, 2.0f));
+    CGFloat angle = atan2f(radialPoint.y, radialPoint.x);
+    return RotaryRingTouchRegionInsideKnob;
 }
 
 @end
